72e343d82e6863a8e51571a340b55d35b1c3e4a9
Hendri Pretorius <pretorh@gmail.com>
2015-05-16 12:19:50 +0200

    use symbol for battery

--- mac/tmux/.tmux/status/batt.sh
+++ mac/tmux/.tmux/status/batt.sh
@@ -1,3 +1,3 @@
 perc=`pmset -g batt | grep % | perl -pe "s/^.+-0\s+(\d+%).+$/\1/"`
 remain=`pmset -g batt | grep % | perl -pe "s/^.*(\d:\d\d) remaining$/\1/"`
-echo "Batt: $perc ($remain) |"
+echo "⚡ $perc ($remain) |"
