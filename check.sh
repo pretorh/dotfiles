41514548209c12a9519f3649008abc574df61231
Hendri Pretorius <pretorh@gmail.com>
2015-05-18 19:31:11 +0200

    when check called with no params, loop over all items

--- check.sh
+++ check.sh
@@ -1,3 +1,13 @@
+if [ -z $1 ] ; then
+    # get all files, ignoring os specific roots, .git and .
+    for d in `find -maxdepth 1 -type d | grep -v arch | grep -v mac | grep -ve "\.git$" | grep -ve "^\.$"` ;
+    do
+        # remove leading ./
+        $0 ${d:2}
+    done
+    exit
+fi
+
 echo "comparing $1/ vs ~/"
 for f in `find $1 -type f | grep -v ".swp$" | sed -e "s/^$1\///"` ; do
     echo "$f"
