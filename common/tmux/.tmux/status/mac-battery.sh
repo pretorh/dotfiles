d76edc1712844fe7e7cd1995599573951d541257
Hendri Pretorius <pretorh@gmail.com>
2016-06-05 18:27:52 +0200

    show battery or power depending on current power source

--- common/tmux/.tmux/status/mac-battery.sh
+++ common/tmux/.tmux/status/mac-battery.sh
@@ -1,3 +1,11 @@
 perc=`pmset -g batt | grep % | perl -pe "s/^.+-0\s+(\d+%).+$/\1/"`
 remain=`pmset -g batt | grep % | perl -pe "s/^.*(\d:\d\d) remaining.*$/\1/"`remain
-echo "⚡ $perc $remain |"
+
+from=`pmset -g batt | grep "drawing from" | perl -pe "s/^.*from '(.+) Power'$/\1/"`
+if [ "$from" == "Battery" ] ; then
+    from="🔋 ";
+elif [ "$from" == "AC" ] ; then
+    from="⚡ ";
+fi
+
+echo "$from $perc $remain |"
