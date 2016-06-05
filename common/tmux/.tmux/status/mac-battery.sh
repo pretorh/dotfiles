155aa255cbb7654c1c6726345c8380b4e627f468
Hendri Pretorius <pretorh@gmail.com>
2016-06-05 18:31:40 +0200

    check if no battery estimate present

--- common/tmux/.tmux/status/mac-battery.sh
+++ common/tmux/.tmux/status/mac-battery.sh
@@ -1,5 +1,8 @@
 perc=`pmset -g batt | grep % | perl -pe "s/^.+-0\s+(\d+%).+$/\1/"`
-remain=`pmset -g batt | grep % | perl -pe "s/^.*(\d:\d\d) remaining.*$/\1/"`remain
+remain=`pmset -g batt | grep % | perl -pe "s/^.*(\(no estimate\)).*$/\1/"`
+if [ -z "$remain" ] ; then
+    remain=`pmset -g batt | grep % | perl -pe "s/^.*(\d:\d\d) remaining.*$/\1/"`
+fi
 
 from=`pmset -g batt | grep "drawing from" | perl -pe "s/^.*from '(.+) Power'$/\1/"`
 if [ "$from" == "Battery" ] ; then
