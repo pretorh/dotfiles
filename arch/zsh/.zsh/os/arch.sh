b04cd4e0eb82c1e236e1f84a21ed51459552d5db
Hendri Pretorius <pretorh@gmail.com>
2018-09-10 21:57:33 +0200

    zsh: fix home/end/pgup/down keys
    on arch, in terminal and tmux atleast

--- arch/zsh/.zsh/os/arch.sh
+++ arch/zsh/.zsh/os/arch.sh
@@ -1,12 +1,16 @@
 # fix keys
-# http://zshwiki.org/home/zle/emacsandvikeys
-# http://zshwiki.org/home/zle/bindkeys
+# by using zsh /usr/share/zsh/functions/Misc/zkbd
+# https://stackoverflow.com/a/10377906
+# consider reading from ~/.zkbd files
 unsetopt MULTIBYTE
 bindkey -v
-bindkey "^[OH" vi-beginning-of-line    # Home
-bindkey "^[OF" vi-end-of-line          # End
+bindkey "^[[H" vi-beginning-of-line    # Home
+bindkey "^[[F" vi-end-of-line          # End
 bindkey '^[[3~' delete-char            # Del
+bindkey "^[[2~" vi-insert              # Insert
 bindkey "^[[1~" vi-beginning-of-line   # Home in tmux
 bindkey "^[[4~" vi-end-of-line         # End in tmux
+bindkey "^[[5~" vi-beginning-of-line   # page-up
+bindkey "^[[6~" vi-end-of-line         # page-down
 
 alias clip="xclip -selection clipboard"         # alias clip to xlip, write to "normal" clipboard
