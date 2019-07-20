0bf7d4eb73819752705a7a6fd455bcc5cf6314f6
Hendri Pretorius <pretorh@gmail.com>
2019-07-20 10:43:41 +0200

    tmux: extract address to ping

--- common/tmux/.tmux/status/ping.sh
+++ common/tmux/.tmux/status/ping.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
+ping_address=1.1.1.1
+
 timeout_param='-w'
 if [ "$(uname)" = 'Darwin' ] ; then
     timeout_param='-t'
 fi
 
-ping_time=$(ping -c 1 1.1.1.1 $timeout_param 1 | grep icmp_seq || echo "offline")
+ping_time=$(ping $ping_address -c 1 $timeout_param 1 | grep icmp_seq || echo "offline")
 ping_msec=$(echo "$ping_time" | sed 's|.*time=\(.*\)$|\1|')
 
 echo "$ping_msec"
