aa655c1da8a5265f364cbf5129b48412609b773b
Hendri Pretorius <pretorh@gmail.com>
2019-07-20 10:39:24 +0200

    tmux: use -w on linux for ping timeout param
    -t on max, -w on linux

--- common/tmux/.tmux/status/ping.sh
+++ common/tmux/.tmux/status/ping.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
-ping_time=$(ping -c 1 1.1.1.1 -t 1 | grep time || echo "offline")
+timeout_param='-w'
+if [ "$(uname)" = 'Darwin' ] ; then
+    timeout_param='-t'
+fi
+
+ping_time=$(ping -c 1 1.1.1.1 $timeout_param 1 | grep time || echo "offline")
 ping_msec=$(echo "$ping_time" | sed 's|.*time=\(.*\)$|\1|')
 
 echo "$ping_msec"
