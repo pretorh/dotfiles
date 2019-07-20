9372f15ac95507120b246a40907b974a3b520cff
Hendri Pretorius <pretorh@gmail.com>
2019-07-20 10:31:43 +0200

    tmux: setup ping check

--- common/tmux/.tmux/status/right.sh
+++ common/tmux/.tmux/status/right.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 battery=$(~/.tmux/status/battery.sh)
+ping=$(~/.tmux/status/ping.sh)
 now=$(date "+%Y-%m-%d %H:%M:%S")
 
-echo "#H | $battery $now"
+echo "#H | $battery $ping | $now"
