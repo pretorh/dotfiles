3b0abb5a4c222e65296310e6b62997a003e0c8dd
Hendri Pretorius <pretorh@gmail.com>
2019-07-20 10:35:29 +0200

    tmux: get ping time to 1.1.1.1

--- common/tmux/.tmux/status/ping.sh
+++ common/tmux/.tmux/status/ping.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-echo "to-check"
+ping_time=$(ping -c 1 1.1.1.1 -t 1 | grep time || echo "offline")
+ping_msec=$(echo "$ping_time" | sed 's|.*time=\(.*\)$|\1|')
+
+echo "$ping_msec"
