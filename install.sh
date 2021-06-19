de8696994b66e182b3d28ecccf9f2dae30ccb9f5
Hendri Pretorius <hendri@pretorh.xyz>
2021-06-19 12:38:37 +0200

    install: allow running pre/post install hook files

--- install.sh
+++ install.sh
@@ -9,6 +9,14 @@ echo "installing into $dest"
 mkdir -pv ~/.gnupg
 mkdir -pv ~/.zsh
 
+hook() {
+  hook="$1"
+  [ ! -x "$name/$hook.sh" ] && return
+
+  echo "$name: $hook"
+  "$name/$hook.sh" "$dest"
+}
+
 install_in_dir() {
   dird="$(dirname "$1")"
   name="$(basename "$1")"
@@ -16,7 +24,12 @@ install_in_dir() {
   echo "$name ..."
   trap 'echo -e "$name \033[1;31m☓ failed\033[0m"' ERR
   pushd "$dird" >/dev/null
-  stow --target "$dest" --verbose --ignore '\.swp$' "$name"
+  hook "pre-install"
+  stow --target "$dest" --verbose \
+    --ignore '\.swp$' \
+    --ignore '^(pre|post)-install.sh$' \
+    "$name"
+  hook "post-install"
   popd >/dev/null
   trap - ERR
   echo -e "$name \033[1;32m✓\033[0m"
