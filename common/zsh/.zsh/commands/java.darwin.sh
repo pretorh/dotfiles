06f21b860ee453ab167d4ba3bbfcefe5057b062e
Hendri Pretorius <pretorh@gmail.com>
2021-07-15 15:26:30 +0200

    zsh: use Android Studio's java as JAVA_HOME on mac

---
+++ common/zsh/.zsh/commands/java.darwin.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# use java included in Android Studio (https://stackoverflow.com/a/44138853/1016377)
+export JAVA_HOME='/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home'
