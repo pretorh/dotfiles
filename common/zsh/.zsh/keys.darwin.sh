a164cbcc0d94328a0b0b8c5389fe285df2c808e1
Hendri Pretorius <pretorh@gmail.com>
2021-07-15 15:46:54 +0200

    zsh: remove bindkeys on mac

--- common/zsh/.zsh/keys.darwin.sh
+++ common/zsh/.zsh/keys.darwin.sh
@@ -1,5 +1,2 @@
-bindkey '^[[1~' vi-beginning-of-line
-bindkey '^[[5~' vi-beginning-of-line
-bindkey '^[[4~' vi-end-of-line
-bindkey '^[[6~' vi-end-of-line
-bindkey '^[[3~' delete-char
+#!/usr/bin/env zsh
+# noop
