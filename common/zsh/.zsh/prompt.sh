434d5de4ff9ebcfa00296f27cf93c360bf35866f
Hendri Pretorius <pretorh@gmail.com>
2022-07-15 16:58:39 +0200

    zsh: include the shell level in prompt
    include the shell level for nested shells in the prompt

--- common/zsh/.zsh/prompt.sh
+++ common/zsh/.zsh/prompt.sh
@@ -18,8 +18,19 @@ _prompt_pwd="%{$fg[cyan]%}%3~"
 # colon, trailing space
 _prompt_char=" : "
 
+_prompt_shell_level() {
+    if [ -z $TMUX ] && [ $SHLVL -gt 1 ] ; then
+        # not running tmux and SHLVL>1
+        echo "%{$fg[cyan]%}[sh@$((SHLVL - 1))] "
+    elif [ $SHLVL -gt 2 ] ; then
+        # tmux is running in zsh, and zsh in tmux pane
+        # works even when nested sh starts tmux (level still starts at 2 in tmux)
+        echo "%{$fg[cyan]%}[sh@$((SHLVL - 2))] "
+    fi
+}
+
 # combine
-PROMPT="$_prompt_exit_status $_prompt_host $_prompt_pwd%{$reset_color%}$_prompt_char"
+PROMPT="$_prompt_exit_status $(_prompt_shell_level)$_prompt_host $_prompt_pwd%{$reset_color%}$_prompt_char"
 
 # shellcheck disable=SC2034,1087,2154,SC2016
 RPROMPT='%(?.%{$fg[green]%}.%{$fg[red]%}:/ )% ${last_command_time}%{$reset_color%}'
