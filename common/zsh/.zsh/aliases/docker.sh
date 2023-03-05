ac8883ca5b7edfb6f0403dda34490b7becfac5be
Hendri Pretorius <pretorh@gmail.com>
2023-03-05 19:39:29 +0200

    zsh: mount current dir in /mnt/app in drun alias

--- common/zsh/.zsh/aliases/docker.sh
+++ common/zsh/.zsh/aliases/docker.sh
@@ -1,3 +1,3 @@
-alias drun='docker run --rm -ti -v $(pwd):/app'
+alias drun='docker run --rm -ti -v $(pwd):/mnt/app'
 alias dbuild="docker build -t tmp ."
 alias ddangling="docker volume ls -qf dangling=true"
