e9425638ed8595574370545a44fdce4a02fd5566
Hendri Pretorius <pretorh@gmail.com>
2025-05-01 11:21:32 +0200

    tmux: add indicator when prefix key is used

--- common/tmux/.tmux/status/left.sh
+++ common/tmux/.tmux/status/left.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# based on https://stackoverflow.com/a/15308651/1016377
 
+# prefix key used: set left-status background to black
+echo -n '#{?client_prefix,#[bg=black],}'
+
+# finally the session name
 echo '#{=4:session_name} '
